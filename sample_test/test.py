57a76e4ba09ee2f63ddd6a4715c4e2fa24a39861
GyeonginKim <oscar.kim@live.co.kr>
2024-05-20 17:29:02 +0900

    edit test.py

---
+++ sample_test/test.py
@@ -0,0 +1,48 @@
+# 동영상 강의 
+# https://www.youtube.com/watch?v=zAkAZjmnS3c&list=PLU9-uwewPMe05-khW3YcDEaHMk_qA-7lI&index=3
+
+# a = 0x8ff
+# print(a)
+
+# a = 3
+# b = 4
+# print(a ** b)
+
+# print(7 % 3)
+
+# 문자열
+
+# a = "Life is too short, You need Python"
+# b = "a"
+# c = "123"
+# print(type(b))
+
+# head = "Python"
+# tail = " is fun!"
+# print(head + tail)
+
+# a = "python"
+# print(a * 2)
+
+
+# multistring.py
+# print("=" * 50)
+# print("My Program")
+# print("=" * 50)
+
+# 문자열 길이 구하기
+# a = "Life is too short"
+# print(len(a))
+
+# 문자열 인덱싱 : 0번부터 시작한다
+# a = "Life is too short, You need Python"
+# print(a[-1])
+# print(a[-34])
+
+# 문자열 슬라이싱
+a = "Life is too short, You need Python"
+# b = a[0] + a[1] + a[2] + a[3]
+# a[이상 : 미만 : 간격]
+b = a[0:4]
+print(b)
+
